01e92e51c49f8ebf549e3967d36fa7610ffbaac6
GouravSinghBesh <110739751+GouravSinghBesh@users.noreply.github.com>
2022-10-01 18:28:39 +0530

    2 files modified

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import loading from './loading.gif'
+import React from 'react';
 
-const Spinner = ()=> {
-    return (
-      <div className='text-center'>
-            <img src={loading} alt="loading" />
-      </div>
-    )
-}
-export default Spinner
+const Spinner = () => {
+  return (
+    <div className="text-center">
+      <img src="./loading.gif" alt="loading" />
+    </div>
+  );
+};
+export default Spinner;
